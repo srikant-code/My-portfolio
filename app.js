310c954db893ff9d3189b633e5aecdeb441c9d73
Srikant Sahoo <srikantsahoo3601@gmail.com>
2020-04-13 01:36:09 +0530

    Major Update

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 AOS.init({
   duration: 1200,
 })
+
+$("#tile-1 .nav-tabs a").click(function() {
+    var position = $(this).parent().position();
+    var width = $(this).parent().width();
+      $("#tile-1 .slider").css({"left":+ position.left,"width":width});
+  });
+  var actWidth = $("#tile-1 .nav-tabs").find(".active").parent("li").width();
+  var actPosition = $("#tile-1 .nav-tabs .active").position();
+  $("#tile-1 .slider").css({"left":+ actPosition.left,"width": actWidth});
+  
